4804d5454add2a25e1bfad6283daeab4012b0590
사내교육용 공용계정 <HAEEDU@aes.hkmg.global>
2020-02-07 13:37:44 +0900

    Add rent, driving info VO

---
+++ src/main/java/hae/basic/vo/DrivingInfoVO.java
@@ -0,0 +1,80 @@
+package hae.basic.vo;
+
+/**
+ * @ClassName   : DrivingInfoVO.java
+ * @Description : 주행 관련 정보 vo 클래스
+ * @author 임현아
+ * @since 2020.02.07
+ * @version 1.0
+ * @see
+ * @Modification Information
+ * <pre>
+ *     since          author              description
+ *  ===========    =============    ===========================
+ *  2020. 2. 7.        임현아                                      주행 관련 정보 vo 클래스
+ * </pre>
+ */
+public class DrivingInfoVO {
+
+    /** 대여 번호 */
+    private String rentNo;
+    
+    /** 위도 */
+    private String latitude;
+    
+    /** 경도 */
+    private String longitude;
+    
+    /** 시간 */
+    private String timeStamp;
+    /**
+     * @return the rentNo
+     */
+    public String getRentNo() {
+        return rentNo;
+    }
+    /**
+     * @param rentNo the rentNo to set
+     */
+    public void setRentNo(String rentNo) {
+        this.rentNo = rentNo;
+    }
+    /**
+     * @return the latitude
+     */
+    public String getLatitude() {
+        return latitude;
+    }
+    /**
+     * @param latitude the latitude to set
+     */
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+    /**
+     * @return the longitude
+     */
+    public String getLongitude() {
+        return longitude;
+    }
+    /**
+     * @param longitude the longitude to set
+     */
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+    /**
+     * @return the timeStamp
+     */
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+    /**
+     * @param timeStamp the timeStamp to set
+     */
+    public void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+	
+}
---
+++ src/main/java/hae/basic/vo/RentVO.java
@@ -0,0 +1,92 @@
+package hae.basic.vo;
+
+/**
+ * @ClassName   : RentVO.java
+ * @Description : 대여 관련 정보 vo 클래스
+ * @author 임현아
+ * @since 2020.02.07
+ * @version 1.0
+ * @see
+ * @Modification Information
+ * <pre>
+ *     since          author              description
+ *  ===========    =============    ===========================
+ *  2020. 2. 7.        임현아                                   대여 관련 정보 vo 클래스
+ * </pre>
+ */
+public class RentVO {
+    
+    /** 대여 번호 */
+    private String rentNo;
+    /** 차량 식별 번호 */
+    private String carNo;
+    /** 사용자 아이디 */
+    private String userID;
+    /** 대여 날짜 */
+    private String rentDate;
+    /** 반납 날짜 */
+    private String returnDate;
+    /**
+     * @return the rentNo
+     */
+    public String getRentNo() {
+        return rentNo;
+    }
+    /**
+     * @param rentNo the rentNo to set
+     */
+    public void setRentNo(String rentNo) {
+        this.rentNo = rentNo;
+    }
+    /**
+     * @return the carNo
+     */
+    public String getCarNo() {
+        return carNo;
+    }
+    /**
+     * @param carNo the carNo to set
+     */
+    public void setCarNo(String carNo) {
+        this.carNo = carNo;
+    }
+    /**
+     * @return the userID
+     */
+    public String getUserID() {
+        return userID;
+    }
+    /**
+     * @param userID the userID to set
+     */
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+    /**
+     * @return the rentDate
+     */
+    public String getRentDate() {
+        return rentDate;
+    }
+    /**
+     * @param rentDate the rentDate to set
+     */
+    public void setRentDate(String rentDate) {
+        this.rentDate = rentDate;
+    }
+    /**
+     * @return the returnDate
+     */
+    public String getReturnDate() {
+        return returnDate;
+    }
+    /**
+     * @param returnDate the returnDate to set
+     */
+    public void setReturnDate(String returnDate) {
+        this.returnDate = returnDate;
+    }
+    
+    
+	
+}
