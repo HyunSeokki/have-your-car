98cc0570ada0a68b941654bd44e6b171499b72de
HyunSeokki <hhs250@naver.com>
2020-02-10 09:05:51 +0900

    Add Active and ActiveLog Service

---
+++ src/main/java/hae/basic/service/ActiveLogService.java
@@ -0,0 +1,64 @@
+package hae.basic.service;
+
+import java.util.List;
+
+import hae.basic.vo.ActiveLogVO;
+
+/**
+ * <pre>
+ * Statements
+ * </pre>
+ *
+ * @ClassName   : ActiveLogService.java
+ * @Description : 클래스 설명을 기술합니다.
+ * @author OHS
+ * @since 2020. 2. 10.
+ * @version 1.0
+ * @see
+ * @Modification Information
+ * <pre>
+ *     since          author              description
+ *  ===========    =============    ===========================
+ *  2020. 2. 10.     OHS     	최초 생성
+ * </pre>
+ */
+
+public interface ActiveLogService {
+    /**
+     * 시동Log정보 등록
+     * @param vo
+     * @return
+     * @throws Exception
+     */
+    void insertActiveLog(ActiveLogVO vo) throws Exception;
+    
+    /**
+     * 시동Log정보 수정
+     * @param vo
+     * @throws Exception
+     */
+    void updateActiveLog(ActiveLogVO vo) throws Exception;
+    
+    /**
+     * 시동Log정보 삭제
+     * @param startNo
+     * @throws Exception
+     */
+    void deleteActiveLog(String startNo) throws Exception;
+    
+    /**
+     * 시동Log정보 조회
+     * @param startNo
+     * @return
+     * @throws Exception
+     */
+    ActiveLogVO selectActiveLog(String startNo) throws Exception;
+    
+    /**
+     * 시동Log정보 목록 조회
+     * @param vo
+     * @return
+     * @throws Exception
+     */
+    List<ActiveLogVO> selectActiveLogList(ActiveLogVO vo) throws Exception;
+}
---
+++ src/main/java/hae/basic/service/impl/ActiveServiceimpl.java
@@ -0,0 +1,64 @@
+package hae.basic.service.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import able.com.service.HService;
+
+import org.springframework.stereotype.Service;
+
+import hae.basic.service.ActiveService;
+import hae.basic.service.dao.ActiveDAO;
+import hae.basic.vo.ActiveVO;
+
+/**
+ * <pre>
+ * Statements
+ * </pre>
+ *
+ * @ClassName   : ActiveServiceimpl.java
+ * @Description : 클래스 설명을 기술합니다.
+ * @author OHS
+ * @since 2020. 2. 10.
+ * @version 1.0
+ * @see
+ * @Modification Information
+ * <pre>
+ *     since          author              description
+ *  ===========    =============    ===========================
+ *  2020. 2. 10.     OHS     	최초 생성
+ * </pre>
+ */
+
+@Service("activeService")
+public class ActiveServiceimpl extends HService implements ActiveService{
+    /** MDAO 등록 */
+    @Resource(name = "ActiveDAO")
+    private ActiveDAO activeDAO;
+    
+    @Override
+    public void insertActive(ActiveVO vo) throws Exception{
+        activeDAO.insertActive(vo);
+    }
+    
+    @Override
+    public void updateActive(ActiveVO vo) throws Exception{
+        activeDAO.updateActive(vo);
+    }
+    
+    @Override
+    public void deleteActive(String carNo) throws Exception{
+        activeDAO.deleteActive(carNo);
+    }
+    
+    @Override
+    public ActiveVO selectActive(String carNo) throws Exception{
+        return activeDAO.selectActive(carNo);
+    }
+    
+    @Override
+    public List<ActiveVO> selectActiveList(ActiveVO vo) throws Exception{
+        return activeDAO.selectActiveList(vo);
+    }
+}
